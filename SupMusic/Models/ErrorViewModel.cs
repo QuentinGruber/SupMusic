7dba9bcc3dd09d5be9afd6adf6113c20851683f4
Quentin Gruber <quentingruber@gmail.com>
2021-01-26 11:08:13 +0100

    Initial commit

---
+++ SupMusic/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SupMusic.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
