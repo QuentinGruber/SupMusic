077c7b7b6e810ef92ae0dc8cc6f7f1b3537de2eb
Quentin Gruber <quentingruber@gmail.com>
2021-01-27 19:05:25 +0100

    models

---
+++ Models/PlaylistModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SupMusic.Models
+{
+    public class PlaylistModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public Boolean isPrivate { get; set; }
+    }
+}
---
+++ Models/SongModel.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace SupMusic.Models
+{
+    public class SongModel
+    {
+        public int ID { get; set; }
+        public int CategoryID { get; set; }
+        public string Name { get; set; }
+        public int Duration { get; set; }
+    }
+}
