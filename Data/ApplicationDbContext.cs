94bb98746da56606bc9c100ed1f1f2779b5b527c
Quentin Gruber <quentingruber@gmail.com>
2021-01-30 10:52:49 +0100

    setup migration

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using SupMusic.Models;
 
 namespace SupMusic.Data
 {
@@ -11,6 +13,50 @@ namespace SupMusic.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+        public DbSet<PlaylistModel> Playlist { get; set; }
+        public DbSet<SongModel> Song { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<IdentityUser>(n =>
+            {
+                // clé primaire
+                n.HasKey(p => p.Id);
+            });
+
+            modelBuilder.Entity<PlaylistModel>(n =>
+            {
+                // clé primaire
+                n.HasKey(p => p.ID);
+
+                // id auto-increment non null
+                n.Property(f => f.ID).ValueGeneratedOnAdd();
+
+                // taille maxi
+                n.Property(p => p.Name).HasMaxLength(64);
+
+                // clé étrangère (song id)
+                n.HasMany<SongModel>()
+                .WithOne()
+                .HasForeignKey(fk => fk.ID)
+                .IsRequired();
+
+            });
+
+            modelBuilder.Entity<SongModel>(n =>
+            {
+                // clé primaire
+                n.HasKey(p => p.ID);
+
+                // id auto_increment non null
+                n.Property(f => f.ID).ValueGeneratedOnAdd();
+
+                // taille maxi
+                n.Property(p => p.Name).HasMaxLength(64);
+            });
         }
     }
 }
