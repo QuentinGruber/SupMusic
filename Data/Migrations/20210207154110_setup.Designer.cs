4341b175f8a4d972fba2a2cfa5b84bd145dee845
Quentin Gruber <quentingruber@gmail.com>
2021-02-07 16:46:21 +0100

    test theme :D

--- Data/Migrations/20210207154110_setup.Designer.cs
+++ Data/Migrations/20210207154110_setup.Designer.cs
@@ -10,7 +10,7 @@ using SupMusic.Data;
 namespace SupMusic.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20210207150527_setup")]
+    [Migration("20210207154110_setup")]
     partial class setup
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
